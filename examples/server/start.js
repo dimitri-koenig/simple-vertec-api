72e6ab1d02e8f64ae8556a0b89d84d28d277e699
Dimitri König <dimitri@dimitrikoenig.net>
2015-08-13 15:05:00 +0200

    Added one basic example (#19)

--- examples/server/start.js
+++ examples/server/start.js
@@ -5,6 +5,8 @@ var SimpleVertecApi = require('simple-vertec-api').SimpleVertecApi;
 var api = new SimpleVertecApi(config.vertec.url, config.vertec.username, config.vertec.password);
 var restify = require('restify');
 var server = restify.createServer();
+var moment = require('moment');
+var _ = require('lodash');
 
 server.use(restify.queryParser());
 server.use(restify.gzipResponse());
@@ -16,6 +18,49 @@ server.use(function crossOrigin(req,res,next){
 	return next();
 });
 
+server.get('/alleleistungenvonheute', function(req, res, next) {
+	var start_time = new Date().getTime();
+
+	api.select(
+		{
+			ocl: 'Leistung',
+			sqlwhere: "(datum >= {ts ':today 00:00:00'}) and (datum <= {ts ':today 23:59:59'})"
+		},
+		{
+			today: moment().format('YYYY-MM-DD')
+		},
+		[
+			'wertInt',
+			'wertExt'
+		]
+	).then(function(response) {
+		var offeneleistungen = response.OffeneLeistung ? response.OffeneLeistung : [];
+		var verrechneteleistungen = response.VerrechneteLeistung ? response.VerrechneteLeistung : [];
+
+		if (_.isPlainObject(offeneleistungen)) {
+			offeneleistungen = [
+				offeneleistungen
+			];
+		}
+
+		if (_.isPlainObject(verrechneteleistungen)) {
+			verrechneteleistungen = [
+				verrechneteleistungen
+			];
+		}
+
+		var alleleistungen = verrechneteleistungen.concat(offeneleistungen);
+
+		res.json({
+			requestTime: new Date().getTime() - start_time,
+			total: alleleistungen.length,
+			leistungen: alleleistungen
+		});
+	}).catch(function(e) {
+		res.json(500, e);
+	});
+});
+
 server.listen(8080, function() {
 	console.log('%s listening at %s', server.name, server.url);
-});
+})
