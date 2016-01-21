b1f6c1ebc63fcde2a12dc8cde180c31729b6a6cf
Dimitri König <dimitri@dimitrikoenig.net>
2016-01-21 14:08:37 +0100

    Adapted tests for multi select feature to own multiSelect() area

--- test/simple-vertec-api.test.js
+++ test/simple-vertec-api.test.js
@@ -381,44 +381,6 @@ describe('SimpleVertecApi', () => {
             compareFilteredString(buildXmlSpy.returnValues.shift(), '<?xml version="1.0" encoding="UTF-8"?><Envelope><Header><BasicAuth><Name>my-username</Name><Password>my-password</Password></BasicAuth></Header><Body><Query><Selection><ocl>where-x-expression = 123 and where-y-expression = &apos;2015-09-21&apos;</ocl></Selection><Resultdef><member>normal-field 123</member><expression><alias>foobar-123</alias><ocl>object.field-2015-09-21</ocl></expression></Resultdef></Query></Body></Envelope>');
         });
 
-        it('combines an array of selection arguments into a single request', () => {
-            sinon.stub(api, 'doRequest');
-
-            let firstQuery = [
-                'where-x-expression = :id and where-y-expression = ":date"',
-                {
-                    id: 123,
-                    date: '2015-09-21'
-                },
-                [
-                    'normal-field :id',
-                    {
-                        alias: 'foobar-:id',
-                        ocl:   'object.field-:date'
-                    }
-                ]
-            ];
-
-            let secondQuery = [
-                {
-                    objref: [
-                        12345,
-                        23456
-                    ]
-                },
-                [
-                    'first-field',
-                    'second-field'
-                ]
-            ];
-
-            api.select([
-                firstQuery,
-                secondQuery
-            ]);
-            compareFilteredString(buildXmlSpy.returnValues.shift(), '<?xml version="1.0" encoding="UTF-8"?><Envelope><Header><BasicAuth><Name>my-username</Name><Password>my-password</Password></BasicAuth></Header><Body><Query><Selection><ocl>where-x-expression = 123 and where-y-expression = &apos;2015-09-21&apos;</ocl></Selection><Resultdef><member>normal-field 123</member><expression><alias>foobar-123</alias><ocl>object.field-2015-09-21</ocl></expression></Resultdef></Query><Query><Selection><objref>12345</objref><objref>23456</objref></Selection><Resultdef><member>first-field</member><member>second-field</member></Resultdef></Query></Body></Envelope>');
-        });
-
         it('catches multiple equal requests and returns one promise', (done) => {
             var resolveCount = 0;
 
@@ -470,6 +432,78 @@ describe('SimpleVertecApi', () => {
                 expect(_.size(api.storedPromises)).to.equal(0);
                 done();
             });
+        });
+    });
+
+    describe('multiSelect()', () => {
+        it('combines an array of selection arguments into a single request', () => {
+            sinon.stub(api, 'doRequest');
+
+            let firstQuery = [
+                'where-x-expression = :id and where-y-expression = ":date"',
+                {
+                    id: 123,
+                    date: '2015-09-21'
+                },
+                [
+                    'normal-field :id',
+                    {
+                        alias: 'foobar-:id',
+                        ocl:   'object.field-:date'
+                    }
+                ]
+            ];
+
+            let secondQuery = [
+                {
+                    objref: [
+                        12345,
+                        23456
+                    ]
+                },
+                [
+                    'first-field',
+                    'second-field'
+                ]
+            ];
+
+            api.multiSelect([
+                firstQuery,
+                secondQuery
+            ]);
+            compareFilteredString(buildXmlSpy.returnValues.shift(), '<?xml version="1.0" encoding="UTF-8"?><Envelope><Header><BasicAuth><Name>my-username</Name><Password>my-password</Password></BasicAuth></Header><Body><Query><Selection><ocl>where-x-expression = 123 and where-y-expression = &apos;2015-09-21&apos;</ocl></Selection><Resultdef><member>normal-field 123</member><expression><alias>foobar-123</alias><ocl>object.field-2015-09-21</ocl></expression></Resultdef></Query><Query><Selection><objref>12345</objref><objref>23456</objref></Selection><Resultdef><member>first-field</member><member>second-field</member></Resultdef></Query></Body></Envelope>');
+        });
+
+        it('throws an error if first and only argument is not an array', () => {
+            sinon.stub(api, 'doRequest');
+            var selectSpy = sinon.spy(api, 'multiSelect');
+
+            try {
+                api.multiSelect({foo: 'bar'});
+            } catch (e) {
+                // we only need the finally block
+            } finally {
+                expect(selectSpy.exceptions).to.have.length(1);
+                expect(selectSpy.exceptions.shift().message).to.have.string('1453380632');
+            }
+
+            try {
+                api.multiSelect('something');
+            } catch (e) {
+                // we only need the finally block
+            } finally {
+                expect(selectSpy.exceptions).to.have.length(1);
+                expect(selectSpy.exceptions.shift().message).to.have.string('1453380632');
+            }
+
+            try {
+                api.multiSelect('something');
+            } catch (e) {
+                // we only need the finally block
+            } finally {
+                expect(selectSpy.exceptions).to.have.length(1);
+                expect(selectSpy.exceptions.shift().message).to.have.string('1453380632');
+            }
         });
     });
 
